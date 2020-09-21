4303a70067c8632321e60f12d2865c37efc1c49e
Julio R.R <juliorivera.2092@gmail.com>
2020-09-21 17:33:13 -0500

    Enlace Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCmMJLiFMpYTVXWCvFxB3AJKeYYJPbQ_5g",
+    authDomain: "e-commerce-uni1.firebaseapp.com",
+    databaseURL: "https://e-commerce-uni1.firebaseio.com",
+    projectId: "e-commerce-uni1",
+    storageBucket: "e-commerce-uni1.appspot.com",
+    messagingSenderId: "818059095235",
+    appId: "1:818059095235:web:23ab893c367d505f344676",
+    measurementId: "G-0JV3QVQHXL"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCmMJLiFMpYTVXWCvFxB3AJKeYYJPbQ_5g",
+    authDomain: "e-commerce-uni1.firebaseapp.com",
+    databaseURL: "https://e-commerce-uni1.firebaseio.com",
+    projectId: "e-commerce-uni1",
+    storageBucket: "e-commerce-uni1.appspot.com",
+    messagingSenderId: "818059095235",
+    appId: "1:818059095235:web:23ab893c367d505f344676",
+    measurementId: "G-0JV3QVQHXL"
+  }
 };
 
 /*
